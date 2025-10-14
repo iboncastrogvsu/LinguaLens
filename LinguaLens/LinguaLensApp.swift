7b28c4c03d13cb1c508a711c481b28f5784076b7
iboncas <castroli@mail.gvsu.edu>
2025-10-14 09:22:32 -0400

    Initial Commit

---
+++ LinguaLens/LinguaLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguaLensApp.swift
+//  LinguaLens
+//
+//  Created by Ibon Castro Llorente on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LinguaLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
